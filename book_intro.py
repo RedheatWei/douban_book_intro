8d1ab87d953059839563fa696d894e6d25e31a6c
RedheatWei <qjyyn@qq.com>
2018-07-12 01:40:23 +0800

    collect urls

--- book_intro.py
+++ book_intro.py
@@ -7,12 +7,15 @@
 '''
 from bs4 import BeautifulSoup
 import requests
+import sqlite3
+from concurrent.futures import ThreadPoolExecutor
 
+proxies = {'http':'111.155.116.210:8123','https':'202.104.113.35:53281'}
 
 class OpenUrl(object):
     @staticmethod
-    def get_boot_tag():
-        response = requests.get('https://book.douban.com/tag/?view=type&icn=index-sorttags-all')
+    def get_book_tag():
+        response = requests.get('https://book.douban.com/tag/?view=type&icn=index-sorttags-all',proxies=proxies)
         soup = BeautifulSoup(response.content, 'lxml')
         try:
             tables = soup.find_all(name='table',class_='tagCol')
@@ -25,8 +28,8 @@ class OpenUrl(object):
         except AttributeError as e:
             print(e)
     @staticmethod
-    def get_book_urls(tag, start_num):
-        response = requests.get("https://book.douban.com/tag/%s?start=%s&type=T" % (tag, start_num))
+    def get_book_urls(**kwargs):
+        response = requests.get("https://book.douban.com/tag/%s?start=%s&type=T" % (kwargs['tag'], kwargs['start_num']),proxies=proxies)
         soup = BeautifulSoup(response.content, 'lxml')
         try:
             subject_item = soup.find_all(name='li', class_='subject-item')
@@ -36,5 +39,46 @@ class OpenUrl(object):
             print(e)
             return False
 
+class ProcessDb(object):
+    def __init__(self,dbname):
+        self.conn = sqlite3.connect(dbname)
+        self.cursor = self.conn.cursor()
+    #插入获取的url到数据库
+    def insertUrl(self,url_list,tag):
+        for url in url_list:
+            sql = 'insert or ignore into urls(url,tag) values("%s","%s")' % (url,tag)
+            self.cursor.execute(sql)
+    def selectTag(self):
+        sql = 'SELECT tag FROM urls GROUP BY tag ORDER BY id'
+        return self.cursor.execute(sql).fetchall()
 
-tag = OpenUrl.get_boot_tag()
+    def __del__(self):
+        self.cursor.close()
+        self.conn.commit()
+        self.conn.close()
+
+class Collect(object):
+    @staticmethod
+    def read_tags():
+        db = ProcessDb("douban.db3")
+        tags = db.selectTag()
+        tag_list = [tag[0] for tag in tags]
+        return tag_list
+    @staticmethod
+    def urls():
+        tag_list = OpenUrl.get_book_tag()
+        end_tag = Collect.read_tags()
+        tag_list = list(set(tag_list).difference(set(end_tag)))
+        for tag in tag_list:
+            for num in range(0,5001,20):
+                kwargs = {"tag":tag,"start_num":num}
+                print(kwargs)
+                urls = OpenUrl.get_book_urls(**kwargs)
+                if urls:
+                    db = ProcessDb("douban.db3")
+                    db.insertUrl(urls,tag)
+                else:
+                    break
+Collect().urls()
+
+# print(Collect.read_tags())
